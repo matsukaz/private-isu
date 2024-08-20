e8ffa1060733fc81988a76c2f5af95bcb30d5ef8
matsukaz <matsukaz@gmail.com>
2024-08-20 23:56:07 +0900

    スロークエリの切り替えをローカル環境から実行できるように修正

--- scripts/profile/slow_query.sh
+++ scripts/profile/slow_query.sh
@@ -3,14 +3,14 @@
 set -eu
 
 on() {
-    mysql -uroot -proot -e "set global slow_query_log_file = '/var/log/mysql/mysql-slow.log';"
-    mysql -uroot -proot -e "set global long_query_time = 0;"
-    mysql -uroot -proot -e "set global slow_query_log = ON;"
+    mysql -uroot -proot -h 127.0.0.1 -e "set global slow_query_log_file = '/var/log/mysql/mysql-slow.log';"
+    mysql -uroot -proot -h 127.0.0.1 -e "set global long_query_time = 0;"
+    mysql -uroot -proot -h 127.0.0.1 -e "set global slow_query_log = ON;"
 }
 
 off() {
-    mysql -uroot -proot -e "set global long_query_time = 10;"
-    mysql -uroot -proot -e "set global slow_query_log = OFF;"
+    mysql -uroot -proot -h 127.0.0.1 -e "set global long_query_time = 10;"
+    mysql -uroot -proot -h 127.0.0.1 -e "set global slow_query_log = OFF;"
 }
 
 if [ "$1" = "off" ]; then
